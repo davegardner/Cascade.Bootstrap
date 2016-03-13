8312231354d2919474bcb5f875b95d1a455103c5
dave <dave@cascadepixels.com.au>
2016-03-14 10:58:06 +1100

    Added Browser Update check

---
+++ Scripts/BrowserUpdate.js
@@ -0,0 +1,8 @@
+﻿var $buoop = { vs: { i: 10, f: 20, o: 12.1, s: 5.1 }, c: 2 };
+function $buo_f() {
+	var e = document.createElement("script");
+	e.src = "//browser-update.org/update.min.js";
+	document.body.appendChild(e);
+};
+try { document.addEventListener("DOMContentLoaded", $buo_f, false) }
+catch (e) { window.attachEvent("onload", $buo_f) }
