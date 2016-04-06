6bcab39565bae96d7aa547dbfe54295649e05259
dave <dave@cascadepixels.com.au>
2016-04-06 17:12:55 +1000

    CBCA theme updates

--- MainMenu.cs
+++ MainMenu.cs
@@ -23,17 +23,12 @@ namespace Cascade.Bootstrap {
         public void GetMenu(IContent menu, NavigationBuilder builder) {
             var workContext = _orchardServices.WorkContext;
             var bootstrapSettings = workContext.CurrentSite.As<BootstrapThemeSettingsPart>();
-            
+
+           
             if (menu.As<TitlePart>().Title == "Main Menu")
             {
-                var maxPosition = _contentManager
-                    .Query<MenuPart, MenuPartRecord>()
-                    .Where(x => x.MenuId == menu.Id)
-                    .List()
-                    .Select(x => GetFirstInteger(x.MenuPosition))
-                    .Max();
-
-                var itemCount = maxPosition + 1;
+                var menuParts = _contentManager.Query<MenuPart, MenuPartRecord>().Where(x => x.MenuId == menu.Id).List();
+                var itemCount = menuParts.Select(x => GetFirstInteger(x.MenuPosition)).Max() + 1;
 
                 //do we want to display admin menu?
                 if (bootstrapSettings.ShowLogInLinksInMenu)
@@ -41,12 +36,18 @@ namespace Cascade.Bootstrap {
                     if (_orchardServices.WorkContext.CurrentUser != null)
                     {
 
-                        builder.Add(T(_orchardServices.WorkContext.CurrentUser.UserName), itemCount.ToString(), item => item.Url("#").AddClass("menuUserName"));
+                        builder.Add(T(FirstWord(_orchardServices.WorkContext.CurrentUser.UserName, bootstrapSettings)), itemCount.ToString(), item => item.Url("#").AddClass("menuUserName"));
                         builder.Add(T("Change Password"), itemCount.ToString() + ".1", item => item.Action("ChangePassword", "Account", new { area = "Orchard.Users" }));
                         builder.Add(T("Sign Out"), itemCount.ToString() + ".2", item => item.Action("LogOff", "Account", new { area = "Orchard.Users", ReturnUrl = _orchardServices.WorkContext.HttpContext.Request.RawUrl }));
                         if (_orchardServices.Authorizer.Authorize(Orchard.Security.StandardPermissions.AccessAdminPanel))
                         {
                             builder.Add(T("Dashboard"), itemCount.ToString() + ".3", item => item.Action("Index", "Admin", new { area = "Dashboard" }));
+
+                            // HACK: for CBCA, add a Help menu item to the menu
+                            if (bootstrapSettings.Swatch == "cbca")
+                            {
+                                builder.Add(T("Help"), itemCount.ToString() + ".4", item => item.Url("Help"));
+                            }
                         }
                     }
 
@@ -69,6 +70,15 @@ namespace Cascade.Bootstrap {
                     result = Int32.Parse(ints[0]);
             }
             return result;
+        }
+
+        // Make user name display more compact for CBCA menu
+        private string FirstWord(string name, BootstrapThemeSettingsPart bootstrapSettings)
+        {
+            var len = name.IndexOf(' ');
+            if (len > 0 && bootstrapSettings.Swatch == "cbca")
+                return name.Substring(0, len);
+            return name;
         }
 
         public string MenuName { get { return "Main Menu"; } }
